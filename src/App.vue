5c8dde5a947ba74aa41b438f7e5acd6b66b98ffb
Ryan Dewick <ryandewick@Ryans-MacBook-Pro-2.local>
2022-12-01 16:48:05 +0000

    background added - maybe fix

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,4 @@ import HomeView from "./views/HomeView.vue";
   </div>
 </template>
 
-<style>
-#app {
-  background: url("src/assets/images/mobile_bg.png");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-}
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-view">
+    <!-- <img src="/src/assets/images/mobile_bg.png" alt="" /> -->
     <Banner />
     <MainTextArea class="text_area" />
   </div>
@@ -16,7 +17,18 @@ export default {
 <style scoped lang="scss">
 .home-view {
   width: 100%;
+  // position: relative;
 }
+
+// img {
+//   position: absolute;
+//   width: 120%;
+//   aspect-ratio: 1;
+//   // min-height: 100%;
+//   // transform: scale(1.2);
+//   top: 0;
+//   right: 0;
+// }
 
 .text_area {
   margin: 0 auto;
