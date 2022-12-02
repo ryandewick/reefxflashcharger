96669ba909aaf5de6d0195aa8f0af78851a8db92
Ryan Dewick <ryandewick@Ryans-MacBook-Pro-2.local>
2022-12-02 13:35:53 +0000

    desktop background added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home-view">
-    <!-- <img src="/src/assets/images/mobile_bg.png" alt="" /> -->
+    <div class="background">
+      <img class="mobile" src="/src/assets/images/mobile_bg.png" />
+      <img class="desktop" src="/src/assets/images/desktop_bg.png" />
+    </div>
     <Banner />
     <MainTextArea class="text_area" />
   </div>
@@ -20,17 +23,35 @@ export default {
   // position: relative;
 }
 
-// img {
-//   position: absolute;
-//   width: 120%;
-//   aspect-ratio: 1;
-//   // min-height: 100%;
-//   // transform: scale(1.2);
-//   top: 0;
-//   right: 0;
-// }
+.mobile {
+  position: absolute;
+  height: 100vh;
+  top: 0;
+  right: 0;
+  filter: contrast(70%);
+}
+
+.desktop {
+  display: none;
+  position: absolute;
+  height: 100vh;
+  top: 0;
+  right: 0;
+  z-index: -1;
+  filter: contrast(70%);
+}
 
 .text_area {
   margin: 0 auto;
+}
+
+@media (min-width: 1000px) {
+  .mobile {
+    display: none;
+  }
+
+  .desktop {
+    display: block;
+  }
 }
 </style>
